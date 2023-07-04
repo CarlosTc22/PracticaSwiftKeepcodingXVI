3888c2fc3af7f721b13cd1c6300fdde8e5a3330e
Carlos Torrejon <carlostc22@gmail.com>
2023-07-04 13:21:21 +0200

    feat: Reservation

--- DBHotel.playground/Contents.swift
+++ DBHotel.playground/Contents.swift
@@ -5,3 +5,14 @@ struct Client {
     let age: Int
     let height: Int
 }
+
+// Reservation: ID único, nombre del hotel, lista de clientes, duración (días), precio,opción de desayuno (true/false).
+
+struct Reservation {
+    let id: Int
+    let hotelName: String
+    let clients: [Client]
+    let duration: Int
+    let price: Double
+    let breakfast: Bool
+}
